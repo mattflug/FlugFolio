459c71340327a39d2c39e0dffaaae50d6ca55b89
Ghost <mbflug@gmail.com>
2023-05-22 14:32:25 -0600

    through landing completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import Navbar from "./scenes/NavBar";
-import { useState } from "react";
+import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing";
+import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
   const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []); 
 
   return (
     <div className="app bg-deep-blue">
       <Navbar
+        isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
     </div>
   );
 }
