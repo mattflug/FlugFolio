e8ed96a5749cf9660eed31b459c95ef8de03398a
Ghost <mbflug@gmail.com>
2023-05-22 16:54:23 -0600

    skills section complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Navbar from "./scenes/NavBar";
 import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
+import MySkills from "./scenes/MySkills";
+import LineGradient from "./components/LineGradient";
 import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
+import { motion } from "framer-motion";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
   const [isTopOfPage, setIsTopOfPage] = useState(true);
-  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+  const isDesktop = useMediaQuery("(min-width:1060px)");
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,7 +24,7 @@ function App() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []); 
 
-  return (
+ return (
     <div className="app bg-deep-blue">
       <Navbar
         isTopOfPage={isTopOfPage}
@@ -29,16 +32,32 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <div className="w-5/6 mx-auto md:h-full">
-        {isAboveMediumScreens && (
+        {isDesktop && (
           <DotGroup
             selectedPage={selectedPage}
             setSelectedPage={setSelectedPage}
           />
         )}
-
-        <Landing setSelectedPage={setSelectedPage} />
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("home")}
+        >
+          <Landing setSelectedPage={setSelectedPage} />
+        </motion.div>
       </div>
-    </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full ">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("skills")}
+        >
+          <MySkills />
+        </motion.div>
+      </div>
+     <LineGradient />
+     </div>
   );
 }
 
