28bf3f637d08b9d76e60273eefad42e8132a65e5
Ghost <mbflug@gmail.com>
2023-05-25 15:01:32 -0600

    save for now

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -20,13 +20,12 @@ const Project = ({ title }) => {
     bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
+
   return (
     <motion.div variants={projectVariant} className="relative">
       <div className={overlayStyles}>
         <p className="text-2xl font-playfair">{title}</p>
         <p className="mt-7">
-          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
-          porttitor accumsan tincidunt.
         </p>
       </div>
       <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
