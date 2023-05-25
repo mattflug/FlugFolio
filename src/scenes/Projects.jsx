ad6dbda9518dedc0b5a6c66e81962dd7d2e2def3
Ghost <mbflug@gmail.com>
2023-05-25 15:29:31 -0600

    fixing

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -14,7 +14,7 @@ const projectVariant = {
   hidden: { opacity: 0, scale: 0.8 },
   visible: { opacity: 1, scale: 1 },
 };
-
+//
 const Project = ({ title }) => {
   const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
     bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
